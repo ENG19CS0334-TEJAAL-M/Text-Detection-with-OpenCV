00feb1236140e40bef7c5935ff11b40bb513f5f9
ENG19CS0334-TEJAAL-M <eng19cs0334.tejaalm@gmail.com>
2022-05-03 12:54:56 +0530

    done few changes

--- TextDetect.py
+++ TextDetect.py
@@ -12,18 +12,18 @@ img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 ##print(pytesseract.image_to_boxes(img))
 
 
-# ##----- to detect characters
-# ##size of image
-# hImg,wImg,_ =img.shape
-# boxes=pytesseract.image_to_boxes(img)
-# for b in boxes.splitlines():
-#     print(b)
-#     b=b.split(' ')
-#     ## transformed to list
-#     print(b)
-#     x,y,w,h=int(b[1]),int(b[2]),int(b[3]),int(b[4])
-#     cv2.rectangle(img,(x,hImg-y),(w,hImg-h),(0,255,0),2)
-#     cv2.putText(img,b[0],(x,hImg-y+25),cv2.FONT_HERSHEY_COMPLEX,1,(50,50,255),2)
+##----- to detect characters
+##size of image
+hImg,wImg,_ =img.shape
+boxes=pytesseract.image_to_boxes(img)
+for b in boxes.splitlines():
+    print(b)
+    b=b.split(' ')
+    ## transformed to list
+    print(b)
+    x,y,w,h=int(b[1]),int(b[2]),int(b[3]),int(b[4])
+    cv2.rectangle(img,(x,hImg-y),(w,hImg-h),(0,255,0),2)
+    cv2.putText(img,b[0],(x,hImg-y+25),cv2.FONT_HERSHEY_COMPLEX,1,(50,50,255),2)
 
 #
 # ## detecting words
